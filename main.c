51b6176ede53afd96e4688021fcdc0d1dfef21a1
HB74C21 <6720@holbertonstudents.com>
2023-12-12 00:56:54 -0800

    Add function fork to main file

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	pid_t pid_number;
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	ssize_t bytesread;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO)) /* Vérifie si le shell tourne en mode "interactif" */
+		{
+
+			printf("hsh $ ");
+		}
+
+		pid_number = fork();
+
+		bytesread = getline(&buffer, &bufsize, stdin);
+		
+		if (bytesread == -1) 
+		{
+			printf("\n");
+			break;
+		}
+
+		if (!strcmp(buffer, "exit\n"))
+			break;
+
+		
+		if (pid_number < 0)
+			printf("Error Fork\n");
+
+		if (pid_number == 0)
+		{
+			printf("I am the son, my pid is %d\n", pid_number);
+		}
+		else
+		{
+			printf("I am the father, my pid is %d\n", pid_number);
+		}
+
+	}
+	return (0);
+}
