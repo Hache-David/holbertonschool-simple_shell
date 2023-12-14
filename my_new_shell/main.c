73d3a205fb15e2bf1fd763ea438b0a3eeda49ee0
Hache-David <Hache-David@github.com>
2023-12-14 13:38:09 +0000

    Add new repository with new main folder for new version of mini-shell.

--- my_new_shell/main.c
+++ my_new_shell/main.c
@@ -1,13 +1,18 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <string.h>
+extern char **environ;
 
 int main(void)
 {
     char *buffer = NULL;
     size_t buffer_size = 0;
     ssize_t prompt;
-    
+	pid_t pid;
+	char *argv[] = {"/bin/ls", "-l", NULL}; // tokéniser ça //
+	int check_exec;
+
     while(1)
     {
         printf("##FirstShell $ ");
@@ -18,11 +23,29 @@ int main(void)
             printf("\n");
             break;
         }
+
         // check if command is true //
+		//--> tokenisation ://
 
-        // fork my parent process //
+		// fork my parent process //
+		pid = fork();
 
-        
+		if (pid == -1)
+		{
+			return (-1);
+		}
+		if (pid == 0) /* in child process */
+		{
+			check_exec = execve(argv[0] , argv, NULL);
+			if (check_exec == -1)
+				perror("./shell: No such file or directory");
+		}
+		else /* in parent process */
+		{
+			wait(NULL);
+		}
+
+
     }
     return (0);
 }
