50d72cd8ef2a4b67c640c0777c1882a419977e09
HB74C21 <6720@holbertonstudents.com>
2023-12-17 15:50:57 -0800

    Refactor shell code, introduce print_environment(), handle cd command, and streamline command execution.

--- main.c
+++ main.c
@@ -1,91 +1,73 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
-#include <stddef.h>
-extern char **environ;
-
 /**
  * main - entry point
  *
- *
- *
  * Return: 0 always succes.
- *
 */
 int main(void)
 {
-	pid_t pid_number;
 	size_t bufsize = 0;
 	ssize_t bytesread;
-	char *args[] = {"ls", NULL};
-	size_t length = 0;
 	char *buffer = NULL;
 	char *buffer_copy;
 	size_t index;
 	int return_value = 0;
+	size_t length;
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO)) /* Vérifie si le shell tourne en mode "interactif" */
+		if (isatty(STDIN_FILENO))
 		{
 			printf("hsh $ ");
+			fflush(stdout);
 		}
 
-		bytesread = getline(&buffer, &bufsize, stdin); /* copie input in buffer */
-
+		bytesread = getline(&buffer, &bufsize, stdin);
 		if (bytesread == -1)
 		{
 			printf("\n");
 			break;
 		}
+
 		buffer[bytesread - 1] = '\0';
-		while (buffer[length] != ' ' && buffer[length] != '\0') /* length of first command */
+
+		length = 0;
+
+		while (buffer[length] != ' ' && buffer[length] != '\0')
 			length++;
 
-    	buffer_copy = malloc(sizeof(char) * (length + 1));
-
+		buffer_copy = malloc(sizeof(char) * (length + 1));
 		if (buffer_copy == NULL)
-			return (0);
+		{
+			perror("malloc");
+			return (EXIT_FAILURE);
+		}
 
-    	for (index = 0; index < length; index++) /* copy command on buffer_copy */
+		for (index = 0; index < length; index++)
 			buffer_copy[index] = buffer[index];
 
+		buffer_copy[length] = '\0';
+
 		return_value = PATH_analyse(buffer_copy);
-
-		if(return_value == 1)
+		if (return_value == 1)
 		{
-			if (!strcmp(buffer, "exit"))
+			if (!strcmp(buffer_copy, "exit"))
 				break;
-
-			if (!strcmp(buffer, "env"))
+			else if (!strcmp(buffer_copy, "env"))
+				print_environment();
+			else if (!strcmp(buffer_copy, "cd"))
 			{
-				char **env = environ;
-				while (*env != NULL)
-				{
-					printf("%s\n", *env);
-					env++;
-				}
-			}
-			pid_number = fork();
-			if (pid_number < 0)
-			{
-				printf("Error Fork\n");
-				exit(EXIT_FAILURE);
-			}
-			if (pid_number == 0)
-			{
-				execve(return_PATH(buffer_copy), args, environ);
-				perror("execve");
-        		exit(EXIT_FAILURE);
+				if (chdir(buffer + length + 1) != 0)
+					perror("cd");
 			}
 			else
-			{
-				waitpid(pid_number, NULL, 0);
-			}
+				execute_command(buffer);
 		}
+		else
+			printf("%s: command not found\n", buffer_copy);
+
+		free(buffer_copy);
 	}
+	free(buffer);
 	return (0);
 }
