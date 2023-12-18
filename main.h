32ecbbededa306ea5aa865fd17c8afafd8bddd2e
HB74C21 <6720@holbertonstudents.com>
2023-12-17 17:08:31 -0800

    Add new prototypes

--- main.h
+++ main.h
@@ -1,8 +1,18 @@
 #ifndef __MINISHELL__
 #define __MINISHELL__
 
-int PATH_analyse(char *buffer_copy);
-char *return_PATH(char *buffer_copy);
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <dirent.h>
+extern char **environ;
 
+int PATH_analyse(char *buffer);
+void execute_command(char *buffer);
+void print_environment(void);
 
 #endif
+
